d671212cf3ec5dc145e630f7407c38f946df6240
Artur Kraev <ravenox@gmail.com>
2016-11-14 19:53:18 +0300

    fix

--- cmd/utils_test.go
+++ cmd/utils_test.go
@@ -1,7 +1,14 @@
 package main
 
 import "io/ioutil"
+import "os"
 
 func createTempDir() (string, error) {
+	err := os.MkdirAll("test", os.ModeDir)
+
+	if err != nil {
+		return "", err
+	}
+
 	return ioutil.TempDir("test", "jsonmerge_test")
 }
